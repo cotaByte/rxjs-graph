68dee2ea18ef75058267804c70b4dc66a2ad1b18
cotaByte <minanajoverrafa@gmail.com>
2025-02-20 20:10:21 +0100

    feature/ make test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponentTestComponent } from './features/component-test/component-test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ComponentTestComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rxjs-graph';
--- src/app/features/component-test/component-test.component.ts
+++ src/app/features/component-test/component-test.component.ts
@@ -1,11 +1,44 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { combineLatest, map, merge, shareReplay, Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-component-test',
-  imports: [],
+  selector: 'component-test',
+  imports: [AsyncPipe],
   templateUrl: './component-test.component.html',
-  styleUrl: './component-test.component.scss'
+  styleUrl: './component-test.component.scss',
 })
 export class ComponentTestComponent {
+  //#region A
+  a = new Subject<string>();
+  a$ = this.a.pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  //#endregion A
 
+  //#region B
+  b = new Subject<string>();
+  b$ = this.b.pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  //#endregion B
+
+  //#region C
+  c = new Subject<string>();
+  c$ = this.c.pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  //#endregion C
+
+  //#region D
+  d = new Subject<string>();
+  d$ = this.d.pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  //#endregion D
+
+  //#region MERGE VALUES
+  merged$ = merge(this.a$, this.b$, this.c$, this.d$).pipe(
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+  //#endregion MERGE VALUES
+
+  //#region COMBINED LATEST
+  combinedLatest$ = combineLatest([this.a$, this.b$, this.c$, this.d$]).pipe(
+    map(([a, b, c, d]) => `${a} - ${b} - ${c} - ${d}`),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+  //#endregion COMBINED LATEST
 }
